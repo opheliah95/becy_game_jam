a406ab1ba1aeb5598151389f7719ea3f4b7d6c7d
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2023-12-08 13:40:17 +0000

    move the character

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,27 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     );
 }
 
+fn character_movement (
+    mut characters: Query<(&mut Transform, &Sprite)>,
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>
+) {
+    for(mut transform, _) in &mut characters {
+        if input.pressed(KeyCode::W) {
+            transform.translation.y += 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::S) {
+            transform.translation.y -= 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::A) {
+            transform.translation.x -= 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::D) {
+            transform.translation.x += 100.0 * time.delta_seconds();
+        }
+    }
+}
+
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins
@@ -31,5 +52,6 @@ fn main() {
                 })
                 .build())
     .add_systems(Startup, setup)
+    .add_systems(Update, character_movement)
     .run();
 }

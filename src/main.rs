6bbf85781e97d199a3301898586924402191d9d6
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2023-12-08 12:31:35 +0000

    initial script

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use bevy::prelude::*;
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    let texture = asset_server.load("character.png");
+    // spawn a sprite
+    commands.spawn(
+        SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(100.0, 100.0)),
+                ..default()
+            },
+            texture,
+            ..default()
+        }
+    );
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .add_plugins(DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Bevy Jam Game".into(),
+                        resolution: (640.0, 480.0).into(),
+                        resizable: false,
+                        ..default()
+                    }),
+                ..default()
+                })
+                .build())
+    .add_systems(Startup, setup)
+    .run();
 }
